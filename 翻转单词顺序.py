69e6714171ec74f811b15db5d141a9d5ca867051
Wsssssss <569467716@qq.com>
2019-08-28 20:26:44 +0800

    update

--- 翻转单词顺序.py
+++ 翻转单词顺序.py
@@ -5,3 +5,38 @@
 
 class Solution:
     def ReverseSentence(self, s):
+        if s == None or len(s) <= 0:
+            return ''
+
+        strlist = list(s)
+        strlist = self.Reverse(strlist)
+        begin = 0
+        end = 0
+        newlist = []
+        result = ''
+        while end < len(s):
+            if end == len(s) - 1:
+                newlist.append(self.Reverse(strlist[begin:]))
+                break
+            if strlist[begin] == ' ':
+                begin += 1
+                end += 1
+                newlist.append(' ')
+            elif strlist[end] == ' ':
+                newlist.append(self.Reverse(strlist[begin:end]))
+                begin = end
+            else:
+                end += 1
+
+        for i in newlist:
+            result += ''.join(i)
+        return result
+
+    def Reverse(self, s):
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        return s
